380035cdac8dbf050f9b758df588a6490cc3a9cf
crystall.101600 <crystall.101600@gmail.com>
2025-02-25 10:36:24 +0000

    Added colors along with fetching the different currencies using list and cupertino items.

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,3 +1,5 @@
+import 'package:bitcoin/coin.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PriceScreen extends StatefulWidget {
@@ -8,36 +10,60 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+
+  List<DropdownMenuItem<String>> getCurrencies() {
+    final dropdownItems = <DropdownMenuItem<String>>[];
+    for (var i = 0; i < currenciesList.length; i++) {
+      final currency = currenciesList[i];
+      final newItem = DropdownMenuItem(
+        value: currency, 
+        child: Text(currency),
+      );
+      dropdownItems.add(newItem);
+    }
+    return dropdownItems;
+  }
+
+  List<Widget> getPickerItems(){
+    final pickerItems = <Widget>[];
+    for (var i = 0; i < currenciesList.length; i++){
+      final currency = currenciesList[i];
+      final newItem = Text(currency);
+      pickerItems.add(newItem);
+    }
+    return pickerItems;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 188, 186, 188),
       appBar: AppBar(
-        title: const Text('C O I N  T I C K E R'),
-      ),
+        title: const Center(
+          child: Text(
+            'C O I N   T I C K E R',
+            ),
+          ),
+        ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Padding(
-            padding: const EdgeInsets.fromLTRB(18,18,18,0),
+            padding: const EdgeInsets.fromLTRB(18, 18, 18, 0),
             child: Card(
-              color:  Colors.lightBlueAccent,
+              color: const Color.fromARGB(255, 44, 36, 67),
               elevation: 5,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10),
               ),
               child: const Padding(
-                padding: EdgeInsets.symmetric(
-                  vertical: 15,
-                  horizontal: 28,
-                ),
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 28),
                 child: Text(
                   '1 BTC = ? USD',
                   textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                  ),
+                  style: TextStyle(fontSize: 20, color: Colors.white),
                 ),
               ),
             ),
@@ -46,11 +72,27 @@ class _PriceScreenState extends State<PriceScreen> {
             height: 150,
             alignment: Alignment.center,
             padding: const EdgeInsets.only(bottom: 30),
-            color: Colors.lightBlue,
-            child: null,
+            color: const Color.fromARGB(255, 122, 121, 133),
+            child: CupertinoPicker(
+              itemExtent: 32,
+              onSelectedItemChanged: (selectedIndex) {
+                print(selectedIndex);
+              },
+              children: getPickerItems(),
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+// DropdownButton<String>(
+//               value: selectedCurrency,
+//               items: getCurrencies(),
+//               onChanged: (value) {
+//                 setState(() {
+//                   selectedCurrency = value!;
+//                 });
+//               },
+//             ),
