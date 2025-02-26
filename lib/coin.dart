396430f643e41fe16ef288c18025daa51f3a34b8
crystall.101600 <crystall.101600@gmail.com>
2025-02-26 11:18:42 +0000

    Added method to fetch the live data for the bitcoin in each currency.

--- lib/coin.dart
+++ lib/coin.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -19,13 +22,25 @@ const List<String> currenciesList = [
   'SEK',
   'SGD',
   'USD',
-  'ZAR'
+  'ZAR',
 ];
 
-const List<String> cryptoList = [
-  'BTC',
-  'ETH',
-  'LTC',
-];
+const List<String> cryptoList = ['BTC', 'ETH', 'LTC'];
 
-class CoinData {}
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate/BTC';
+const APIKey = 'b166917b-d885-4ff9-a243-88bdd797ca30';
+
+class CoinData {
+  Future getCoin() async {
+    final requestURL = '$coinAPIURL/USD?apikey=$APIKey';
+    final response = await http.get(Uri.parse(requestURL));
+    if (response.statusCode == 200) {
+      final decodedData = jsonDecode(response.body);
+      final lastPrice = decodedData['rate'];
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+      throw 'Problem';
+    }
+  }
+}
